b128579199d467c710e250e6a0cf39bf5953fe08
bing.yu <957070670@qq.com>
2017-09-25 10:08:31 +0800

    remove unsued test code

--- src/test/java/com/base/distribute/lock/generator/ZookeeperDistributeLockTest.java
+++ src/test/java/com/base/distribute/lock/generator/ZookeeperDistributeLockTest.java
@@ -12,37 +12,7 @@ import com.base.distribute.lock.generator.core.DistributeLockType;
 
 
 public class ZookeeperDistributeLockTest {
-	
-//	@Test
-//	public void testZookeeperDistributeLock() throws InterruptedException{
-//		ExecutorService pool = Executors.newFixedThreadPool(5);
-//		
-//			TestTask testTask1 = new TestTask("TestTaskZookeeper",DistributeLockType.ZOOKEEPER);
-//			Future<Long> future1= pool.submit(testTask1);
-////			TestTask testTask2 = new TestTask("TestTask2");
-////			Future<Long> future2= pool.submit(testTask2);
-//			long costTime1 = 0;
-//		//	long costTime2 = 0;
-//		    try {
-//		    	  costTime1 = future1.get();
-//		    	//  costTime2 = future2.get();
-//			  System.out.println("TestTask1" +" execute" + costTime1/1000.0 +"s");
-////			  System.out.println("TestTask2"  +" execute" + costTime2/1000 +"s");
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			} catch (ExecutionException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		    
-////		    
-//	    Thread.sleep(60 * 1000);
-//		    
-//		    System.out.println("done");
-//		   
-//	}
-	
+
 	
 	@Test
 	public void multiTestZookeeperDistributeLock() throws InterruptedException{
